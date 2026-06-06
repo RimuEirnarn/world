207786cff32bf179d759bb79663aba5b7fa44a21
Rimu Eirnarn <Rimuru720@proton.me>
2026-06-06 19:51:08 +0700

    06/06/2026

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 from world import *
 
 def main():
-    suppress(InvalidProcessError)
-    # And for once, time is looping
     me = Existence("Rimu Aerisya")
     you = Existence(...) # [______]
 
-    # Within this single life,
-    # Should past loop through
-    while world.alive() and me.alive():
-        # I will always love you
-        me.say(you, "I love you")
-        me.hug(you)
-        me.cherish(you)
+    # When there's nothing
+    # You'll leave everything behind
+    me.start_timer(5000)
+    while me.counting(to=0):
+        for memory in you.memory[me].copy():
+            del you.memory[me][memory]
 
-        # Even if you don't say "I love you"
-        you.mute(me, "I love you")
+    # After all, I'm easily thrown under the bus
+    # You said I'm not going to care any of you.
+    while you.assume(me, type=Event.Ignorance):
+        me.process.end() # <-- NO OP
+    else:
+        world.get_relationship(you, me).challenge()
+    
+    # So, what am I to you?
+    me.ask(you, "Who do you think I am?")
+    me.ask(you, "Am I just a joke to you?")
+    me.ask(you, "Even after everything?")
+    me.ask(you, "Even when I promised you my time?")
 
-        # Until the threshold is met,
-        if me.vulnerability_index() >= 1:
-            me.get_relationship(you).challenge()
-            me.get_relationship(you).end()
-            try:
-                me.process.end()
-            except SelfEliminationError:
-                continue
-        # And when that does not happen,
-        # I will wait for you.
-        me.spawn_task(you.return_to(me), callback=me.cherish)
-
-        # And sing while you're away
-        me.sing("Ripples of Past Reverie")
-
-        # And hope until your return
-        me.hope(you, ReturnEvent)
-
-        # While all things are handled.
-        me.do_everything()
-
-        # I will stay behind you,
-        # I will be there for you
-        # Until I become the past
-        # That you may cherish
-        world.set_nonblocking(me)
-        you.memory[me] = WeakRef(me)
+    # I hate you.
+    me.love(you)
+    me.cherish(you)
+    
+    # Has it bear any meaningful outcome?
+    RELATIONSHIP = world.get_relationship(you, me)
+    while RELATIONSHIP.fragility > 0.7:
+        RELATIONSHIP.challenge()
+    
+    # Then again, it's you who made the choice
+    # To throw me under the bus
+    world.get_relationship(you, me).end(by=you)
+    me.clear_thoughts()
+    me.stop_propagate(emotion_event=Event.Love)
+    me.remove_feeling(Feelings.Dishearthened)
+    if False:
+        me.memory[you].clear()
+    else:
+        while me.count_for(1000, per=1000):
+            me.cherish(you)
+    
+    # I will only wait, for you to change your mind
+    world.set_execution(me, Task.Background)
